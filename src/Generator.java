e5e4122fe9298f13745104e9e77593e0986c588b
sschafe <sschafe2@emich.edu>
2021-08-05 21:35:53 -0400

    Start of the program

---
+++ src/Generator.java
@@ -0,0 +1,10 @@
+// Words come from here:
+// https://www.ef.sg/english-resources/english-vocabulary/top-1000-words/
+// Also added some words there myself
+public class Generator {
+
+	public Generator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
