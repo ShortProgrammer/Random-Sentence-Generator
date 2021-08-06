e982a56208c76e62256118a058e4b0f57b1e1b3d
sschafe <sschafe2@emich.edu>
2021-08-05 21:38:32 -0400

    Fixing the Words doc and making them words that I put in

--- src/Generator.java
+++ src/Generator.java
@@ -1,6 +1,4 @@
-// Words come from here:
-// https://www.ef.sg/english-resources/english-vocabulary/top-1000-words/
-// Also added some words there myself
+
 public class Generator {
 
 	public Generator() {
